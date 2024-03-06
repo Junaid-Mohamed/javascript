529c60cdef28eb02528a096b7ec990046a263ba4
Mohammed Junaid <junaid7mohamed@gmail.com>
2024-03-06 23:19:16 +0530

    closures

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 
 //  for call stack.
-var x = 7;
+// var x = 7;
 
-function getName(){
-    console.log("Junaid")
+// function getName(){
+//     console.log("Junaid")
+// }
+
+// getName();
+// console.log(x)
+// console.log(getName)
+
+// // functions
+
+// var y =1;
+// a();
+// b();
+// console.log(y)
+
+// function a(){
+//     var y =10;
+//     console.log(y)
+// }
+
+// function b(){
+//     var y =100
+//     console.log(y)
+// }
+
+// scope & lexical env
+// console.log(x,a);
+// var x = 10;
+
+// let a = 20;
+
+// a = 30;
+
+// console.log(x,a);
+
+// function a(){
+//     var b = 10
+//     bq();
+//     function bq(){
+//         var c=20;
+//     }
+//     console.log(c);
+// }
+
+// a();
+
+// closures.
+
+function x(){
+    var z = 10;
+    function y(){
+        console.log(z);
+    }
+    z=20;
+    return y;
 }
 
-getName();
-console.log(x)
-console.log(getName)
-
-// functions
-
-var y =1;
+var a = x();
 a();
-b();
-console.log(y)
-
-function a(){
-    var y =10;
-    console.log(y)
-}
-
-function b(){
-    var y =100
-    console.log(y)
-}
